23533a89af94a629db537f3e4635622625b61e30
Patrick <patrickrebecchi@hotmail.com>
2025-10-06 20:40:27 -0300

    Utilizando contrutor e Override

--- src/br/com/alura/screenmatch/principal/PrincipalComLista.java
+++ src/br/com/alura/screenmatch/principal/PrincipalComLista.java
@@ -6,12 +6,18 @@ import br.com.alura.screenmatch.modelos.Titulo;
 
 import java.io.File;
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+
 
 public class PrincipalComLista {
     public static void main(String[] args) {
         Filme filme1 = new Filme("O poderoso chefão", 1970);
+        filme1.avalia(10);
         Filme filme2 = new Filme("Avatar", 2023);
+        filme2.avalia(7);
         Filme filme3 = new Filme("Dogville", 2003);
+        filme3.avalia(8);
         Serie lost = new Serie ("Lost", 2000);
 
         ArrayList<Titulo> lista = new ArrayList<>();
@@ -19,9 +25,16 @@ public class PrincipalComLista {
         lista.add(filme2);
         lista.add(filme3);
         lista.add(lost);
-        for (Titulo item: lista){
+        for (Titulo item: lista) {
             System.out.println(item);
+            if (item instanceof Filme filme) {
+
+                System.out.println("Classificacão: " + filme.getClassificacao());
+            }
         }
+
+
+
         
         //System.out.println(lista.size());
 
