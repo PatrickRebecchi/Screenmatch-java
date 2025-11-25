fc3a0792d11c6680c2eb6450756d7b11a30db0eb
Patrick <patrickrebecchi@hotmail.com>
2025-11-25 16:23:41 -0300

    Att projet

--- src/br/com/alura/screenmatch/principal/PrincipalComBusca.java
+++ src/br/com/alura/screenmatch/principal/PrincipalComBusca.java
@@ -5,12 +5,19 @@ import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.util.Scanner;
 
 public class PrincipalComBusca {
     public static void main(String[] args) throws IOException, InterruptedException {
+
+        Scanner leitura = new Scanner(System.in);
+        System.out.println("Digite o filme para busca: ");
+        var busca = leitura.nextLine();
+
+
         HttpClient client = HttpClient.newHttpClient();
         HttpRequest request = HttpRequest.newBuilder()
-                .uri(URI.create("https://www.omdbapi.com/?t=matrix&apikey=225a676f"))
+                .uri(URI.create("https://www.omdbapi.com/?t="+ busca +"&apikey=225a676f"))
                 .build();
 
         HttpResponse<String> response = client
